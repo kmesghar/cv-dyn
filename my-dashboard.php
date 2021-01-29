aca1bbec90b6d29c28764fb1176772202e6a9460
kmesghar <khalid.mesghar@sfr.fr>
2021-01-29 11:32:24 +0100

    update

--- my-dashboard.php
+++ my-dashboard.php
@@ -1,6 +1,10 @@
 <?php
     session_name("my-dynamic-cv");
     session_start();
+
+    $action = "profile";
+    if (isset($_GET["action"]))
+        $action = $_GET["action"];
     
     $page = "my-dashboard";
     include_once "inc/parts/header.php";
@@ -10,30 +14,31 @@
 
         <h2>Mon tableau de bord</h2>
 
-        <nav  class="navbar navbar-light bg-light">
-            <ul class="nav nav-tabs">
+        <ul class="nav nav-tabs">
                 <li class="nav-item">
-                    <a href="?action='profile'" class="nav-link active">Mon profile</a>
+                    <a href="?action=profile" class="nav-link <?php if ($action == "profile") echo "active"; ?>">Mon profile</a>
                 </li>
                 <li class="nav-item">
-                    <a href="?action='profile'" class="nav-link">Mes articles</a>
+                    <a href="?action=articles" class="nav-link <?php if ($action == "articles") echo "active"; ?>">Mes articles</a>
                 </li>
                 <li class="nav-item">
-                    <a href="?action='profile'" class="nav-link">Mes compétences</a>
+                    <a href="?action=competences" class="nav-link <?php if ($action == "competences") echo "active"; ?>">Mes compétences</a>
                 </li>
                 <li class="nav-item">
-                    <a href="?action='profile'" class="nav-link">Mon expérience</a>
+                    <a href="?action=experiences" class="nav-link <?php if ($action == "experiences") echo "active"; ?>">Mon expérience</a>
                 </li>
                 <li class="nav-item">
-                    <a href="?action='profile'" class="nav-link">Ma formation</a>
+                    <a href="?action=formation" class="nav-link <?php if ($action == "formation") echo "active"; ?>">Ma formation</a>
                 </li>
                 <li class="nav-item">
-                    <a href="?action='profile'" class="nav-link">Mes loisirs</a>
+                    <a href="?action=loisirs" class="nav-link <?php if ($action == "loisirs") echo "active"; ?>">Mes loisirs</a>
                 </li>
             </ul>
         </nav>
 
-        
+        <div class="border-left border-bottom pl-2 mb-3">
+        </div>
+
     </main>
 
 <?php
