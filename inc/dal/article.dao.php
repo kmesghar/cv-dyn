786c0823a03947ac471d2da69619e59cf450090b
kmesghar <khalid.mesghar@sfr.fr>
2021-01-31 21:38:42 +0100

    update

---
+++ inc/dal/article.dao.php
@@ -0,0 +1,31 @@
+<?php
+    class ArticleDAO {
+        
+        public static function loadAll(): array {
+            return array();
+        }
+
+        public static function findByKeywords(array $keywords): array {
+            return array();
+        }
+
+        public static function get(int $id): Article {
+            return new Article();
+        }
+
+        public static function save(Article $article): Article {
+            return false;
+        }
+
+        public static function delete(int $id): bool {
+            return true;
+        }
+
+        public static function up(Article $article): bool {
+            return false;
+        }
+
+        public function down(Article $article): bool {
+            return false;
+        }
+    }
