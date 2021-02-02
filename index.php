8f862f3458bf362dc01a763a42c8474d36a2abe6
kmesghar <khalid.mesghar@sfr.fr>
2021-02-02 22:48:10 +0100

    update

--- index.php
+++ index.php
@@ -4,53 +4,32 @@
 
     include_once "inc/class/article.class.php";
     
-    $articles = Article::loadAll();
+    $articles = Article::loadAllPublished();
 
     $page = "home";
     include_once "inc/parts/header.php";
 ?>
     <main class="container mt-3">
-        <h1>Mon CV en ligne dynamique avec porte-folio !</h1>
+        <h1 class="mb-5">Mon CV en ligne dynamique avec porte-folio !</h1>
 
-        <div class="row mt-5">
+        <?php foreach ($articles as $article): ?>
+        <div class="row my-4">
             <div class="col-3">
-                <img src="ressources/img/pc-1660564_960_720.jpg" alt="" class="img-thumbnail rounded">
+                <?php if ($article-> getImage() != ""): ?>
+                    <img src="<?= $article-> getImage(); ?>" alt="<?= $article-> getTitle(); ?>" class="img-thumbnail rounded">
+                <?php endif; ?>
             </div>
             <div class="col-9">
-                <h3>Malin !</h3>
-                Mon CV en ligne est dynamique et me permet de le mettre en jour facilement via une interface de gestion dédiée !
+                <h3><?= $article-> getTitle(); ?></h3>
+                <?php if ($article-> getAbstract() != ""):
+                    echo $article-> getAbstract();
+                else:
+                    echo substr($article-> getContent(), 0, 1000);
+                endif; ?>
             </div>
         </div>
+        <?php endforeach; ?>
 
-        <div class="row mt-2">
-            <div class="col-3">
-                <img src="ressources/img/wordpress-876983_960_720.jpg" alt="" class="img-thumbnail rounded">
-            </div>
-            <div class="col-9">
-                <h3>Développeur !</h3>
-                Agé de 43 ans, je suis un développeur web dynamique et blablabla ...
-            </div>
-        </div>
-
-        <div class="row mt-2">
-            <div class="col-3">
-                <img src="ressources/img/wordpress-876983_960_720.jpg" alt="" class="img-thumbnail rounded">
-            </div>
-            <div class="col-9">
-                <h3>Dynamique !</h3>
-                Agé de 43 ans, je suis un développeur web dynamique et blablabla ...
-            </div>
-        </div>
-
-        <div class="row mt-2">
-            <div class="col-3">
-            <img src="ressources/img/document-5608903_960_720.jpg" alt="" class="img-thumbnail rounded">
-            </div>
-            <div class="col-9">
-                <h3>Mon CV !</h3>
-                <a href="my-cv.php">Voir mon CV !</a> 
-            </div>
-        </div>
     </main>
 
     <?php
